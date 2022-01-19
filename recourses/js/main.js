d7a584ea6aa41e91678c814a665b85bcb734b3a8
Myron Malykhin <myronx2021@gmail.com>
2022-01-19 21:50:50 +0100

    Addig page's first version:)

---
+++ recourses/js/main.js
@@ -0,0 +1,48 @@
+function menu_appear(){
+	document.getElementById('menu').style.left = "0";
+	document.getElementById('bg-cover').style.position = "absolute";
+}
+function menu_disappear(){
+	document.getElementById('menu').style.left = "-30%";
+	document.getElementById('bg-cover').style.position = "";	
+}
+
+// SERVICE ID => service_wnou52g
+// USER ID => user_m2IEGOkBcFPjKt8BeJJhc
+// TEMPLATE ID => template_mkc4x8c
+(function(){
+	emailjs.init("user_m2IEGOkBcFPjKt8BeJJhc");
+})();
+const vue = new Vue({
+    el: '#contact_form',
+    data(){
+    	return {
+        	from_name: '',
+        	from_email: '',
+        	message: '',
+        	contact: '',
+        	subject: '',
+        	}
+    	},
+    methods: {
+        enviar(){
+           	let data = {
+                from_name: this.from_name,
+                message: this.message,
+                contact: this.contact
+            };
+                        
+            emailjs.send("service_wnou52g","template_mkc4x8c", data)
+           	.then(function(response) {
+            	if(response.text === 'OK'){
+                	//alert('El correo se ha enviado de forma exitosa');
+                	window.location.href="contact_with_me.html"
+            	}
+            	console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+            }, function(err) {
+                	alert('Ocurrió un problema al enviar el correo');
+                	console.log("FAILED. error=", err);
+            });
+        }
+    }
+});
